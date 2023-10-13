2c747919658fa1676e5841a7e32703025bc9b675
wjdavis5 <william.davis2@carvana.com>
2023-10-13 10:00:10 -0400

    format

--- Program.cs
+++ Program.cs
@@ -22,14 +22,14 @@ builder.Services.AddBackgroundHealthChecks(builder.Configuration)
         tags: new string[] { "db", "sql", "sqlserver" },
         timeout: TimeSpan.FromSeconds(5)
         )
-        .AddAzureBlobStorage(
-            connectionString: "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;",
-            containerName: "test",
-            name: "AzureStorage")
-        .Services.Configure<HealthCheckPublisherOptions>(options =>
-            {
-                options.Delay = TimeSpan.FromSeconds(2);
-            });
+    .AddAzureBlobStorage(
+        connectionString: "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;",
+        containerName: "test",
+        name: "AzureStorage")
+    .Services.Configure<HealthCheckPublisherOptions>(options =>
+        {
+            options.Delay = TimeSpan.FromSeconds(2);
+        });
 
 var app = builder.Build();
 app.MapBackgroundHealthChecks("/health");
