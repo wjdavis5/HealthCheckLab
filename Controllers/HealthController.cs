126d1a2507233c44541908255936731ac317b2fb
wjdavis5 <william.davis2@carvana.com>
2023-10-13 09:11:08 -0400

    add sql check

--- Controllers/HealthController.cs
+++ Controllers/HealthController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.VisualBasic;
 
 namespace HealthCheckLab.Controllers;
 
@@ -7,6 +9,7 @@ namespace HealthCheckLab.Controllers;
 [Route("[controller]")]
 public class HealthController : ControllerBase
 {
+    //docker run -it -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=yourStrong(!)Password" -p 1433:1433 -d mcr.microsoft.com/mssql/server
 
     private readonly ILogger<HealthController> _logger;
 
@@ -18,6 +21,27 @@ public class HealthController : ControllerBase
     [HttpGet(Name = "Health")]
     public async Task<IActionResult> Get()
     {
-       return new OkObjectResult(new { Status = "Healthy" });
+        var isDbConnected = await CheckDbConnection();
+        return !isDbConnected
+            ? new ObjectResult(new { Status = "Unhealthy" }) { StatusCode = 500 }
+            : (IActionResult)new OkObjectResult(new { Status = "Healthy" });
     }
+
+    private async Task<bool> CheckDbConnection()
+    {
+        var connectionString = "Server=localhost,1433;Database=master;User Id=sa;Password=yourStrong(!)Password;";
+        using var connection = new SqlConnection(connectionString);
+        try
+        {
+            await connection.OpenAsync();
+            await using var cmd = new SqlCommand("SELECT 1", connection);
+            cmd.ExecuteNonQuery();
+            return true;
+        }
+        catch (SqlException)
+        {
+            return false;
+        }
+    }
+
 }
