5b9001dfea449da970b12e74469b92d2dd501ab8
wjdavis5 <william.davis2@carvana.com>
2023-10-13 09:36:08 -0400

    3

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Azure.Storage.Blobs;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,7 +22,11 @@ builder.Services.AddHealthChecks()
         failureStatus: HealthStatus.Unhealthy,
         tags: new string[] { "db", "sql", "sqlserver" },
         timeout: TimeSpan.FromSeconds(5)
-        );
+        )
+        .AddAzureBlobStorage(
+            connectionString: "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;", 
+            containerName: "test", 
+            name: "AzureStorage");
 
 var app = builder.Build();
 app.MapHealthChecks("/health");
