aceba4c21970b014a850bcea520e98ceb674c1a3
wjdavis5 <william.davis2@carvana.com>
2023-10-13 10:51:29 -0400

    timeout

--- Program.cs
+++ Program.cs
@@ -25,7 +25,8 @@ builder.Services.AddHealthChecks()
     .AddAzureBlobStorage(
         connectionString: "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;", 
         containerName: "test", 
-        name: "AzureStorage")
+        name: "AzureStorage",
+        timeout: TimeSpan.FromSeconds(1))
     .AddCheck<SimpleHealthCheck>(
         "Simple", 
         failureStatus: HealthStatus.Degraded, 
