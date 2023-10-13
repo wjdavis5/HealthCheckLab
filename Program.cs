7bc335a8298a24761539387694568ae4a785405f
wjdavis5 <william.davis2@carvana.com>
2023-10-13 09:23:15 -0400

    3

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +8,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//dotnet add package AspNetCore.HealthChecks.SqlServer --version 7.0.0
+
+builder.Services.AddHealthChecks()
+    .AddSqlServer(
+        options: new HealthChecks.SqlServer.SqlServerHealthCheckOptions()
+        {
+            ConnectionString = "Server=localhost,1433;Database=master;User Id=sa;Password=yourStrong(!)Password;TrustServerCertificate=True;Encrypt=false",
+            CommandText = "SELECT 1"
+        },
+        name: "SqlServer",
+        failureStatus: HealthStatus.Unhealthy,
+        tags: new string[] { "db", "sql", "sqlserver" },
+        timeout: TimeSpan.FromSeconds(5)
+        );
 
 var app = builder.Build();
+app.MapHealthChecks("/health");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
